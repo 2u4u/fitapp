a9b17d4ad5339406949319692e5783318f0f666d
U U <u.ulugbek@gmail.com>
2020-01-02 19:11:28 +0300

    removed https

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,15 @@ const proxy = require('http-proxy-middleware');
 //IMPORTANT NOTE: secure false is to fix issue with UNABLE_TO_VERIFY_LEAF_SIGNATURE, need comment it or remove in dev
 
 module.exports = function (app) {
-  //API address
-  app.use(proxy('/api', { target: 'https://[::1]:5000/', secure: false }));
-  //Public address in case of storaing files locally
-  app.use(proxy('/public', { target: 'https://[::1]:5000/', secure: false }));
+  if (process.env.NODE_ENV === "production") {
+    //API address
+    app.use(proxy('/api', { target: 'http://[::1]:5000/' }));
+    //Public address in case of storaing files locally
+    app.use(proxy('/public', { target: 'http://[::1]:5000/' }));
+  } else {
+    //API address
+    app.use(proxy('/api', { target: 'https://[::1]:5000/', secure: false }));
+    //Public address in case of storaing files locally
+    app.use(proxy('/public', { target: 'https://[::1]:5000/', secure: false }));
+  }
 };
